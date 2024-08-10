77579fbb86cb6bb3de0b5950124473d2dbd667ef
Clarissa Ch'ng <clarissachnggg@gmail.com>
2024-08-10 16:13:41 +0800

    fixed horizontal overflow issue

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ jQuery(function($){
 
     // Horizontal Scroll Animation
     $(window).on('scroll', function() {
+		// var textPosition = $(window).scrollTop() / $(window).height() * 100;
         var textPosition = Math.round($(window).scrollTop() / $(window).height() * 60);
         $('.horizontal-scroll').css('transform', 'translateX(' + (textPosition - 130) + '%)');
     });
@@ -61,4 +62,37 @@ jQuery(function($){
 	}
 	
 	changeImage(myElement, imgList);
+
+	// Scroll Speed Animation
+	$.fn.moveIt = function(){
+	
+		var $window = $(window);
+		var instances = [];
+		
+		$(this).each(function(){
+		instances.push(new moveItItem($(this)));
+		});
+		
+		window.onscroll = function(){
+		var scrollTop = $window.scrollTop();
+		instances.forEach(function(inst){
+			inst.update(scrollTop);
+		});
+		}
+	}
+	
+	var moveItItem = function(el){
+		
+		this.el = $(el);
+		this.speed = parseInt(this.el.attr('data-scroll-speed'));
+	};
+	
+	moveItItem.prototype.update = function(scrollTop){
+		this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+	};
+	
+		// Initialization
+	$(function(){
+		$('[data-scroll-speed]').moveIt();
+	});	
 })
